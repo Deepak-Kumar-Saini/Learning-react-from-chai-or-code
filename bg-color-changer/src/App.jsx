59c3db312e2473c1fbbc8511f43257155fbdfb8a
deepak-kumar-saini2 <876467deepak@gmail.com>
2026-07-13 22:03:19 +0530

    create a component

--- bg-color-changer/src/App.jsx
+++ bg-color-changer/src/App.jsx
@@ -1,8 +1,14 @@
+import BgColorBtn from "./components/BgColorBtn"
+
 function App() {
 
   return (
     <>
-    <h1 className="m-2 text-2xl font-semibold bg-amber-100 text-center">Hello world!</h1>
+    <div className="m-1 bg-amber-50 flex rounded-full p-2 absolute left-[50%] bottom-10 transform translate-x-[-50%]">
+      <BgColorBtn />
+      <BgColorBtn />
+      <BgColorBtn />
+    </div>
     </>
   )
 }
---
+++ bg-color-changer/src/components/BgColorBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BgColorBtn() {
+  return (
+    <div className="m-2 bg-[yellow] text-center w-20 p-1 rounded-full">Red</div>
+  )
+}
+
+export default BgColorBtn
