cef4ea20bbe73a9ff779764a33cdc17b92b867d7
deepak-kumar-saini2 <876467deepak@gmail.com>
2026-07-14 11:37:50 +0530

    learn about props

--- bg-color-changer/src/App.jsx
+++ bg-color-changer/src/App.jsx
@@ -5,9 +5,7 @@ function App() {
   return (
     <>
     <div className="m-1 bg-amber-50 flex rounded-full p-2 absolute left-[50%] bottom-10 transform translate-x-[-50%]">
-      <BgColorBtn />
-      <BgColorBtn />
-      <BgColorBtn />
+      <BgColorBtn color="yellow" text="yellow"/>
     </div>
     </>
   )
--- bg-color-changer/src/components/BgColorBtn.jsx
+++ bg-color-changer/src/components/BgColorBtn.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-function BgColorBtn() {
+function BgColorBtn({color,text}) {
+  
   return (
-    <div className="m-2 bg-[yellow] text-center w-20 p-1 rounded-full">Red</div>
+    <div className={`m-2 bg-[${color}] text-center w-20 p-1 rounded-full`}>{text}</div>
+    
   )
 }
 
