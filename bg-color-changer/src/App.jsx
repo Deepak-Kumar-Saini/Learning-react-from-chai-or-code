67e708a97d7a06d88fdc2d91f420c07c908dd886
deepak-kumar-saini2 <876467deepak@gmail.com>
2026-07-14 12:19:49 +0530

    learn how to reuse components

--- bg-color-changer/src/App.jsx
+++ bg-color-changer/src/App.jsx
@@ -5,7 +5,12 @@ function App() {
   return (
     <>
     <div className="m-1 bg-amber-50 flex rounded-full p-2 absolute left-[50%] bottom-10 transform translate-x-[-50%]">
-      <BgColorBtn color="yellow" text="yellow"/>
+
+      <BgColorBtn bGcolor="bg-yellow-500" text="yellow"/>
+      <BgColorBtn bGcolor="bg-blue-500" text="blue"/>
+      <BgColorBtn bGcolor="bg-green-500" text="green"/>
+      <BgColorBtn bGcolor="bg-pink-500" text="pink"/>
+
     </div>
     </>
   )
--- bg-color-changer/src/components/BgColorBtn.jsx
+++ bg-color-changer/src/components/BgColorBtn.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function BgColorBtn({color,text}) {
+function BgColorBtn({bGcolor,text}) { 
+  // bGcolor = `bg-${bGcolor}-500`
   
   return (
-    <div className={`m-2 bg-[${color}] text-center w-20 p-1 rounded-full`}>{text}</div>
-    
+    <div className={`m-2 ${bGcolor} uppercase font-bold border-2 text-center w-20 p-1 rounded-full`}>{text}</div>
   )
 }
 
