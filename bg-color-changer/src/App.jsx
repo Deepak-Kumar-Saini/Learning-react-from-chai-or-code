7a396ccc3161bc99a93ac9b0f5261e5ed862065f
deepak-kumar-saini2 <876467deepak@gmail.com>
2026-07-15 13:30:23 +0530

    add color change logic

--- bg-color-changer/src/App.jsx
+++ bg-color-changer/src/App.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react"
 import BgColorBtn from "./components/BgColorBtn"
 
 function App() {
 
+  const data = useState()
+
   return (
     <>
-    <div className="m-1 bg-amber-50 flex rounded-full p-2 absolute left-[50%] bottom-10 transform translate-x-[-50%]">
+      <div className={`h-screen`} >
+        <div className="m-1 bg-amber-50 flex rounded-full p-2 absolute left-[50%] bottom-10 transform translate-x-[-50%]">
 
-      <BgColorBtn bGcolor="bg-yellow-500" text="yellow"/>
-      <BgColorBtn bGcolor="bg-blue-500" text="blue"/>
-      <BgColorBtn bGcolor="bg-green-500" text="green"/>
-      <BgColorBtn bGcolor="bg-pink-500" text="pink"/>
+          <BgColorBtn bGcolor="bg-yellow-500" text="yellow" fun={data}/>
+          <BgColorBtn bGcolor="bg-blue-500" text="blue" />
+          <BgColorBtn bGcolor="bg-green-500" text="green" />
+          <BgColorBtn bGcolor="bg-pink-500" text="pink" />
 
-    </div>
+        </div>
+      </div>
     </>
   )
 }
--- bg-color-changer/src/components/BgColorBtn.jsx
+++ bg-color-changer/src/components/BgColorBtn.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-function BgColorBtn({bGcolor,text}) { 
-  // bGcolor = `bg-${bGcolor}-500`
-  
+function BgColorBtn({ bGcolor, text }) {
+  const body = document.querySelector('body')
+
+  let colorChange = () => {
+    console.log(text);
+    
+    body.style.backgroundColor = `${text}`;
+  }
+
+
   return (
-    <div className={`m-2 ${bGcolor} uppercase font-bold border-2 text-center w-20 p-1 rounded-full`}>{text}</div>
+    <div onClick={colorChange} className={`m-2 ${bGcolor} uppercase font-bold border-2 cursor-pointer text-center w-20 p-1 rounded-full`}>{text}</div>
   )
 }
 
